1babac75898ebb21eaaaa907823704034976f0a7
Iego Ribeiro <iego_aguiar@hotmail.com>
2021-07-01 21:25:49 -0300

    App10_CadDevComFoto

---
+++ App10_CadDevComFoto/app/src/main/java/com/pdm/app10_caddevcomfoto/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.pdm.app10_caddevcomfoto
+
+enum class Senioridade(val nome: String) {
+    JUNIOR("Junior"),
+    PLENO("Pleno"),
+    SENIOR("Senior")
+}
---
+++ App10_CadDevComFoto/app/src/main/java/com/pdm/app10_caddevcomfoto/Stack.kt
@@ -0,0 +1,7 @@
+package com.pdm.app10_caddevcomfoto
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
